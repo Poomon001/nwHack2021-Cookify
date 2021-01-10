702822f2a77d9a29080c0aee2c9182d868a23a0e
Poomrapee Chuthamsatid <poomon2542@gmail.com>
2021-01-10 11:04:00 -0800

    push to heroku

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import  Flask, render_template, url_for, redirect, request, send_file
+from flask_sqlalchemy import SQLAlchemy
+import recipe
+
+''' create instance of Flask web app '''
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+db.create_all()
+
+''' The decoration is to give a route to Flask to access this method '''
+''' in the route parameter is to give a path to the method (web query) '''
+@app.route("/")
+def home():
+    incomplete = Item.query.filter_by(complete=False).all()
+    return render_template('home.html', incomplete=incomplete)
+
+@app.route('/add', methods=['POST'])
+def add():
+    item = Item(text=request.form['items'], complete=False)
+
+    if (item.text.strip() != ""):
+        db.session.add(item)
+        db.session.commit()
+
+    return redirect(url_for('home'))
+
+@app.route('/complete/<id>')
+def complete(id):
+
+        ID = id
+        if (ID is not None):
+            item = Item.query.filter_by(id=int(ID)).first()
+            item.complete = True
+            db.session.commit()
+
+        return redirect(url_for('home'))
+
+@app.route('/topDishes')
+def topDishes():
+    dishes = top_dish_helper()
+
+    priority = []
+
+    urls = []
+
+    for x in dishes:
+        urls.append(recipe.thumbnail(x[0]["link"]))
+        priority.append(x[1])
+
+    print(urls)
+
+    dishes = [q[0] for q in dishes]
+
+    return render_template('recipe.html', dishes=dishes, urls=urls, pri=priority)
+
+@app.route('/display/<name>')
+def display(name):
+
+    dish = recipe.reverse_lookup(name)
+    print(dish)
+    return render_template('display.html', dish=dish)
+
+
+
+
+
+
+
+def top_dish_helper():
+    incomplete = Item.query.filter_by(complete=False).all()
+    ing = []
+    for i in incomplete:
+        ing.append(i.text)
+    dishes = recipe.recipe_find(ing)
+
+    return dishes
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ recipe.py
@@ -0,0 +1,58 @@
+import json
+
+
+# find all possible recipes that can be made with the current ingredients
+def recipe_find(ingredients):
+
+    # store the recipes from the json file as a list
+    with open("recipebook.json", encoding="utf8") as read_file:
+        data = json.load(read_file)
+
+    recipes = data["recipes"]
+
+    matches = []
+
+    # eliminate human error
+    ingredients = list(map(str.lower, ingredients))
+    ingredients = list(map(str.strip, ingredients))
+
+    count = 0
+
+    # add a recipe to matches if the user has the correct ingredients
+    for x in recipes:
+        count = len(x["ingredients"])
+        x["ingredients"] = list(map(str.lower, x["ingredients"]))
+        c = all(i in ingredients for i in x["ingredients"])
+        if c is True:
+            matches.append([x,0])
+        else:
+            # prioritize recipes in ascending order based on number of missing ingredients
+            for y in x["ingredients"]:
+                if y in ingredients:
+                    count -= 1
+            matches.append([x, count])
+
+    matches.sort(key=lambda z: z[1])
+    matches = [q for q in matches if q[1] < 4]
+    return matches[:3]
+
+
+# find the YouTube thumbnail based on the link of the video
+def thumbnail(url):
+   str = "https://i.ytimg.com/vi/___/maxresdefault.jpg"
+
+   newStr = str.replace("___", url[30:])
+
+   return newStr
+
+
+# find the information of a recipe based on the name
+def reverse_lookup(name):
+    with open("recipebook.json", encoding="utf8") as read_file:
+        data = json.load(read_file)
+
+    recipes = data["recipes"]
+
+    for x in recipes:
+        if x["name"] == name:
+            return x
